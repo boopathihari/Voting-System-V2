0bba43b9b544dc4792f686e4ae012cb265438c47
Boopathi Hari <rboopathi@saleminfotech.com>
2023-01-14 23:42:08 +0530

    Lastchanges

--- src/App.js
+++ src/App.js
@@ -16,10 +16,10 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<WelcomePage />} />
-        <Route path="https://63c2ec49a96e93304ffe0004--tranquil-rabanadas-94dde9.netlify.app/votingPage" element={<VotingPage />} />
-        <Route path="https://63c2ec49a96e93304ffe0004--tranquil-rabanadas-94dde9.netlify.app/voteResult" element={<VoteResult />} />
+        <Route path="/votingPage" element={<VotingPage />} />
+        <Route path="/voteResult" element={<VoteResult />} />
         {/* <Route path="/voteResult" element={<VoteResult />} /> */}
-        <Route path="https://63c2ec49a96e93304ffe0004--tranquil-rabanadas-94dde9.netlify.app/adminPage" element={<AdminPage />} />
+        <Route path="/adminPage" element={<AdminPage />} />
 
       </Routes>
     </BrowserRouter>
