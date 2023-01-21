594ddfb4655069f6e7e0fb96334e3fa824737e64
Boopathi Hari <rboopathi@saleminfotech.com>
2023-01-21 12:45:40 +0530

    loading changes

--- src/Pages/AdminPage/AdminPage.jsx
+++ src/Pages/AdminPage/AdminPage.jsx
@@ -5,6 +5,7 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
+import CircularProgress from '@mui/material/CircularProgress';
 import Paper from '@mui/material/Paper';
 import './AdminPage.scss';
 import { Button } from '@mui/material';
@@ -14,12 +15,16 @@ const BaseUrl = "https://votingvv2.onrender.com/"
 
 const AdminPage = () => {
     const [DataJS,setDataJS] = React.useState([]);
-
+    const [loading, setLoading] = React.useState(true);
     
             React.useEffect(()=>{
                 fetch(BaseUrl+'fetchall')
                 .then(res=>res.json())
-                .then(result=>setDataJS(result))
+                .then(result=>{
+                  setDataJS(result);
+                  setLoading(false);
+                })
+
                 .catch(error=>console.log(error));
             },[]);
 
@@ -36,22 +41,31 @@ const handleDeleteHandler = () =>{
   return (
     <div>
         
+   {loading &&  <CircularProgress />}
 
-       <TableContainer component={Paper} className="TableContainer" sx={{ maxHeight: 500 }}>
-        <div className='BtnContainer' >
-        <Button onClick={handleDeleteHandler} variant="outlined" color="error" className='Btn'>
-            Clear All Data
-            </Button>
-        </div>
+  {!loading && 
+      <TableContainer component={Paper} className="TableContainer" sx={{ maxHeight: 500 }}>
+      <div className='BtnContainer' >
+      <Button onClick={handleDeleteHandler} variant="outlined" color="error" className='Btn'>
+          Clear All Data
+          </Button>
+      </div>
       <Table sx={{ minWidth: 100 }} aria-label="sticky table" stickyHeader>
-        <TableHead sx={{ display: "table-header-group" }}>
-          <TableRow>
-            <TableCell>S.No</TableCell>
-            <TableCell align="right">General Secretary</TableCell>
-            <TableCell align="right">Join Secretary</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
+
+
+
+      <TableHead sx={{ display: "table-header-group" }}>
+
+        <TableRow>
+          <TableCell>S.No</TableCell>
+          <TableCell align="right">General Secretary</TableCell>
+          <TableCell align="right">Join Secretary</TableCell>
+        </TableRow>
+
+      </TableHead>
+      
+
+          <TableBody>
           {DataJS.val?.map((row,index) => (
               <TableRow
               key={row._id}
@@ -67,10 +81,15 @@ const handleDeleteHandler = () =>{
               
           ))}
         </TableBody>
+
+        
       </Table>
-    </TableContainer>
+      </TableContainer>
+  }
+      
       
     </div>
+   
   );
 }
 
--- src/Pages/VoteResultPage.jsx
+++ src/Pages/VoteResultPage.jsx
@@ -5,10 +5,11 @@ import Divider from '@mui/material/Divider';
 import ListItemText from '@mui/material/ListItemText';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Avatar from '@mui/material/Avatar';
+import CircularProgress from '@mui/material/CircularProgress';
 import Typography from '@mui/material/Typography';
 import './VoteResult.styles.scss';
 import { Grid } from '@mui/material';
-const BaseUrl = "https://votingvv2.onrender.com/"
+const BaseUrl = "https://votingvv2.onrender.com/";
 
 
 
@@ -16,7 +17,7 @@ const VoteResult = () => {
     const [dataJS,setDataJS] = React.useState([]);
     const [dataGS,setDataGS] = React.useState([]);
     const [TotalCount,setTotalCount] = React.useState([]);
-
+    const [loading, setLoading] = React.useState(true);
 
     // const TotalCount = VoteApi;
 
@@ -24,7 +25,11 @@ const VoteResult = () => {
     React.useEffect(()=>{
         fetch(BaseUrl+'groupbyJoinSec')
         .then(res=>res.json())
-        .then(result=>setDataJS(result))
+        .then(result=>{
+
+            setDataJS(result);
+            setLoading(false);
+        })
         .catch(error=>console.log(error));
     },[]);
 
@@ -32,7 +37,10 @@ const VoteResult = () => {
     React.useEffect(()=>{
         fetch(BaseUrl+'groupbyGeneral')
         .then(res=>res.json())
-        .then(result=>setDataGS(result))
+        .then(result=>{
+            setDataGS(result);
+            setLoading(false);
+        })
         .catch(error=>console.log(error));
     },[]);
 
@@ -49,12 +57,15 @@ const VoteResult = () => {
   return (
     <div>
 
-
 <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} className="WinnerListContainer">
 
   <Grid item xs={6} >
         <h1 className='GSTitle'><span className='subTitleListGS'>General Secretary </span>List</h1>
         <div className='GSList'>
+
+            {loading &&  <CircularProgress />}
+
+            {!loading && 
             <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }} className="WinnerList">
             {dataGS.map((item,index) => (
                 <div key={item._id}>
@@ -84,8 +95,12 @@ const VoteResult = () => {
                 
             
             </List>
+
+            }
+            
         </div>
-        <h3 className='TotalCounts'>Total voter participation: <span className='Counts'>{TotalCount}</span></h3>
+        
+        {!loading && <h3 className='TotalCounts'>Total voter participation: <span className='Counts'>{TotalCount}</span></h3>}
 
   </Grid>
 
@@ -93,6 +108,9 @@ const VoteResult = () => {
   <Grid item xs={6}  className="WinnerListJS">
         <h1 className='JSTitle'><span className='subTitleListJS'>Joint Secretary </span>List</h1>
         <div className='GSList'>
+        {loading &&  <CircularProgress />}
+
+        {!loading && 
             <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }} className="WinnerList">
                 {dataJS.map((item,index)=>(
                     <div key={item._id}>
@@ -124,6 +142,7 @@ const VoteResult = () => {
                 
               
             </List>
+        }
         </div>
   </Grid>
 </Grid>
